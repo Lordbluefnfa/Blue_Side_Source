06f975234af590e843e83bbd01efba8cb512ccc6
Lordbluefnfa <107429343+Lordbluefnfa@users.noreply.github.com>
2022-08-14 16:23:12 -0300

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -20,7 +20,7 @@ class PauseSubState extends MusicBeatSubstate
 	var grpMenuShit:FlxTypedGroup<Alphabet>;
 
 	var menuItems:Array<String> = [];
-	var menuItemsOG:Array<String> = ['Resume', 'Restart Song', 'Change Difficulty', 'Options', 'Exit to menu'];
+	var menuItemsOG:Array<String> = ['Continuar', 'Recomecar', 'Mudar De Dificuldade', 'OPTIONS', 'Saida para o menu'];
 	var difficultyChoices = [];
 	var curSelected:Int = 0;
 
